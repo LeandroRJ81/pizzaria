ae4c394bbce2cf93c7874befbf9b47f5780f126c
LeandroRJ81 <leandrogt22@hotmail.com>
2025-05-26 20:27:03 -0300

    errors treatment

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
 
+interface IProps{
+  req: Request;
+  res: Response | any; 
+}
+
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    res.json({ message: 'Hello, world!' });
+router.get('/teste', ({req, res}: IProps) => {
+    return res.json({nome: "Leandro"});
   });
 
 export { router };
