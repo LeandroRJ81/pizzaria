fd001f5a3a0ff53df86481e51b80db904393cc0c
LeandroRJ81 <leandrogt22@hotmail.com>
2025-05-27 17:31:53 -0300

    create_users

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma";
 
 interface UserRequest {
     name: string;
@@ -7,8 +8,37 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({name, email, password}: UserRequest) {
-        console.log(name);
-        return{ name: name}
+        
+        //Verifica se ele enviou o email
+        if(!email){
+            throw new Error("Email incorreto")
+        }
+
+        //Verifica se esse email já existe
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists) {
+            throw new Error("Usuário já cadastrado")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password,
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+        return user;
     }
 }
 
