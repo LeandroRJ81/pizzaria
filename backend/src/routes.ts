3fdcdab9e2be29204bc51f7d121ce11f4586ba93
LeandroRJ81 <leandrogt22@hotmail.com>
2025-05-28 11:29:51 -0300

    create-category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,9 +4,13 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
+
+// -- ROTAS USER --
 
 router.post('/users', new CreateUserController().handle)
 
@@ -14,6 +18,10 @@ router.post('/session', new AuthUserController().handle)
 
 router.get('/info', isAuthenticated, new DetailUserController().handle)
 
+// -- ROTAS CATEGORY --
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+
 export { router };
 
-//Linha 6: Ao colocar o 'return', o código apresenta erro no (req, res)...
