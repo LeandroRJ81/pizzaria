66c11b4b1964a24ea1fdced1cee83c0174380dbe
LeandroRJ81 <leandrogt22@hotmail.com>
2025-05-27 21:07:37 -0300

    auth_middleware

---
+++ backend/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,37 @@
+import {Request, Response, NextFunction} from 'express'
+import { verify } from 'jsonwebtoken'
+
+interface PayLoad{
+    sub: string
+}
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+
+    //Receber o token
+    const authToken = req.headers.authorization;
+
+    if(!authToken){
+        res.status(401).end()
+    }
+
+    const[, token] = authToken.split(" ")
+
+    try{
+
+        //validar o token
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as PayLoad
+
+        return next();
+        
+    }catch(err){
+        res.status(401).end()
+    }
+        
+}
