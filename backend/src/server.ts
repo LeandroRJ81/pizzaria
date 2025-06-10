9ea71db80f684f1bc3b2c9f915c6ed8a28d4f1fc
LeandroRJ81 <leandrogt22@hotmail.com>
2025-06-10 13:50:19 -0300

    edit-server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,6 +30,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   })
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server online!')
 })
